ded8901f99d2a23b10dc74f79e5c50f3e20be6ee
Victor Vattier <victor.vattier35@gmail.com>
2022-03-21 13:45:31 +0100

    desktop responsive + css chart done 100%

--- sportsee/src/components/ActivityChart/ActivityChart.jsx
+++ sportsee/src/components/ActivityChart/ActivityChart.jsx
@@ -1,17 +1,18 @@
-import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, } from 'recharts'
-import styled from 'styled-components'
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Bar, } from 'recharts'
+import { BarChartContainer, LegendContainer, LegendItemsContainer, LegendItemContainer, RedDot, BlackDot, LegendTitle } from './ActivityChartStyle'
 
 function ActivityChart( {data} ) {
 
     return (
+        
         <BarChartContainer>
+            <LegendBarChart />
             <BarChart width={702} height={200} data={data}>
                 <CartesianGrid strokeDasharray="1 3" vertical={false}/>
                 <XAxis tickLine={false} tickSize={15} tick={{fill: "#9B9EAC", fontSize: "14px", fontWeight: '500'}}/>
                 <YAxis tick={{fill: "#9B9EAC",fontSize: "14px", fontWeight: '500'}} stroke="none" dataKey={"kilogram"} orientation='right' yAxisId="kilogram" domain={['dataMin -1', 'dataMax + 1']} allowDecimals={false} />
                 <YAxis stroke="none" dataKey={"calories"} yAxisId='calories' domain={['dataMin-100', 'dataMax+100']}/>
-                <Tooltip />
-                <Legend align='right' verticalAlign='top' iconType="circle" iconSize={7} />
+                <Tooltip content={< CustomTooltip />}/>
                 <Bar dataKey="calories" fill="#E60000" barSize={5} radius={[5, 5, 0, 0]} yAxisId="calories"/>
                 <Bar dataKey="kilogram" fill="#282D30" barSize={5}  radius={[5, 5, 0, 0]} yAxisId="kilogram"/>
          </BarChart>
@@ -19,13 +20,39 @@ function ActivityChart( {data} ) {
     )
 }
 
-const BarChartContainer = styled.div`
-    margin:auto;
-    width: 835px;
-    height: 320px;
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 5px;
-    background-color: #FBFBFB;`
+function LegendBarChart() {
+
+    return (
+        <LegendContainer>
+            <LegendTitle>Activité quotidienne</LegendTitle>
+            <LegendItemsContainer>
+                <LegendItemContainer>
+                    <BlackDot></BlackDot>
+                    <p>Poids (kg)</p>
+                </LegendItemContainer>
+                <LegendItemContainer>
+                    <RedDot></RedDot>
+                    <p>Calories brûlées (kCal)</p>
+                </LegendItemContainer>
+            </LegendItemsContainer>
+        </LegendContainer>
+    )
+}
+
+function CustomTooltip( { payload, active }) {
+        if ( active && payload) {
+            return (
+                <div style={{color : "#fff", fontSize: "0.8rem", fontWeight: '700', margin: '0', padding: "1rem"}}>
+                    <p style={{color : "#fff", fontSize: "0.8rem", fontWeight: '700', margin: '0', padding: "1rem", backgroundColor: '#e60000'}}>
+                        {`${payload[0].value} kCal`}
+                    </p>
+                    <p style={{color : "#fff", fontSize: "0.8rem", fontWeight: '700', margin: '0', padding: "1rem", backgroundColor: '#000'}}>
+                        {`${payload[1].value} kg`}
+                    </p>
+                </div>
+            )
+        } return null
+    }
+
+
 export default ActivityChart
---
+++ sportsee/src/components/ActivityChart/ActivityChartStyle.jsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const BarChartContainer = styled.div`
+    position: relative; 
+    margin:auto;
+    width: 835px;
+    height: 320px;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 5px;
+    background-color: #FBFBFB;`
+
+export const LegendContainer = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    margin: 0 0 10px 0;
+
+`
+export const LegendItemsContainer = styled.div`
+    display: flex;
+    margin-right: 26px;
+    `
+export const LegendItemContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 32px;
+    color: #74798C;
+    font-family: Roboto;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 24px;
+    letter-spacing: 0px;
+    text-align: left;
+
+`
+export const BlackDot = styled.div`
+    width: 8px;
+    height: 8px;
+    border-radius: 10px;
+    margin-right: 12px;
+    background-color: black;
+
+`
+export const RedDot = styled.div`
+    width: 8px;
+    height: 8px;
+    margin-right: 12px;
+    background-color: red;
+    border-radius: 10px;
+`
+export const LegendTitle = styled.p`
+    margin-left: 32px;
+    font-family: Roboto;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 24px;
+    letter-spacing: 0px;
+    text-align: left;
+
+    `
